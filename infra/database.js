6853768bc4802e7c58ebea4d0b8751caaefd1df4
wiliamtaesunglee <wiliam.tae@gmail.com>
2025-04-28 12:17:32 -0300

    update database connection and define docker db access to consume from .env file

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,11 @@ import { Client } from "pg";
 
 export async function query(queryObj) {
   const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   })
   client.connect()
   const result = await client.query(queryObj)
